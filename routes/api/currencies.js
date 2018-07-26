86dac6debf839ea3f6d48a736bcc6fa532999ac3
Clifton Shum <cliftonshum@gmail.com>
2018-07-27 03:21:36 +0800

    Modify AWS ElastiCache Redis Endpoint

--- routes/api/currencies.js
+++ routes/api/currencies.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 //Redis client
 var redis = require('redis');
-var client = redis.createClient();
+var client = redis.createClient(6379,'redis-node.gmk625.ng.0001.apne1.cache.amazonaws.com');
 
 const { promisify } = require('util');
 const getAsync = promisify(client.get).bind(client);
